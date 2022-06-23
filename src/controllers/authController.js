e10df410ef6f14a651ed97f85541ec6bf7c17ca1
Radomir Pekotv <Radomirpetkov@abv.bg>
2022-06-23 12:01:17 +0300

    create login functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,20 +7,38 @@ authController.get(`/register`, (req, res) => {
     res.render("auth/register")
 })
 
-authController.post(`/register`, async (req, res)=>{
-    const {username, password, repeatPassword, adress} = req.body
-    if(password !== repeatPassword){
-        return res.render(`auth/register`, {error: "Password doesnt match"})
+authController.post(`/register`, async (req, res) => {
+    const { username, password, repeatPassword, adress } = req.body
+    if (password !== repeatPassword) {
+        return res.render(`auth/register`, { error: "Password doesnt match" })
+    }
+    try {
+        const user = await authService.register(username, password, adress)
+        res.render(`home`)
+    } catch (error) {
+        res.render(`auth/register`, { error })
     }
 
-// const user = await authService.register(username, password, adress)
-res.render(`home`)
 
 })
 
 authController.get(`/login`, (req, res) => {
 
     res.render("auth/login")
+})
+
+authController.post(`/login`, async (req, res) => {
+    const { username, password } = req.body
+    try {
+        const user = await authService.login(username, password)
+        if (user) {
+            res.cookie(`session`, user)
+            res.redirect(`/`)
+        }
+    } catch (error) {
+        res.render(`auth/login`, {error})
+    }
+
 })
 
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,33 @@
 const User = require(`../models/User`)
+const bcrypt = require(`bcrypt`)
+const jwt = require(`jwt-promisify`)
 
-exports.register = async (username, password, adress) =>{
-    
-    const user = await User.create({username, password, adress})
+const jwtPrivateKey = `fgasp13vss1cfsdas`
+const saltRounds = 10
+
+exports.register = async (username, password, adress) => {
+    try {
+        const hasedPassword = await bcrypt.hash(password, saltRounds)
+        const user = await User.create({ username, password: hasedPassword, adress })
+        return user
+    } catch (error) {
+        throw error
+    }
+}
+
+exports.login = async (username, password) =>{
+    const user = await User.findOne({username})
+    if (!user) {
+        throw (`Invalid user`)
+
+    }
+    const isAuth = await bcrypt.compare(password, user.password)
+    if(isAuth){
+        const token = jwt.sign({user}, jwtPrivateKey)
+        return token
+    } else {
+        throw (`Your password doesn't match`)
+    }
+    return;
+
 }
