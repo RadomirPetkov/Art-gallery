1cac398d4dbbfdf9eb1f3ad425882a832ebcfb20
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2022-06-23 17:57:27 +0300

    edit details page

--- src/controllers/publicationsController.js
+++ src/controllers/publicationsController.js
@@ -1,5 +1,5 @@
 const publicationsController = require(`express`).Router()
-const { createPublication } = require(`../services/publicationService`)
+const { createPublication, getOne, getOneDetailed } = require(`../services/publicationService`)
 const { isUser } = require(`../middlewares/authMiddleware`)
 
 publicationsController.get(`/create`, isUser, (req, res) => {
@@ -13,8 +13,20 @@ publicationsController.post(`/create`, isUser, async (req, res) => {
         await createPublication({ ...req.body, author: userId })
         res.redirect(`/gallery`)
     } catch (error) {
-        res.render(`publications/create`, {error})
+        res.render(`publications/create`, { error })
     }
 })
+
+publicationsController.get(`/details/:publicationId`, async (req, res) => {
+    const publicationId = req.params.publicationId
+    const currentPublication = await getOneDetailed(publicationId, "author").lean()
+    const publicationAuthor = currentPublication.author._id
+    const currentUser = req.user?.user._id
+    const isAuthor = publicationAuthor == currentUser
+
+    res.render(`publications/details`, { ...currentPublication, isAuthor, currentUser })
+})
+
+
 
 module.exports = publicationsController
--- src/services/publicationService.js
+++ src/services/publicationService.js
@@ -3,3 +3,7 @@ const Publication = require(`../models/Publication`)
 exports.createPublication = (publicationData) => Publication.create(publicationData)
 
 exports.getAll = () => Publication.find()
+
+exports.getOne = (publicationId) => Publication.findById(publicationId)
+
+exports.getOneDetailed = (publicationId, model) => Publication.findById(publicationId).populate(model)
