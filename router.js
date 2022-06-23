899ab86c788b144f8fa857e3486a9e20bd82deac
Radomir Pekotv <Radomirpetkov@abv.bg>
2022-06-23 11:03:12 +0300

    render register and login page

--- router.js
+++ router.js
@@ -1,7 +1,9 @@
 const router = require("express").Router()
 const homeController = require(`./src/controllers/homeController`)
+const authController = require(`./src/controllers/authController`)
 
 router.get(`/`, homeController)
+router.use(`/auth`, authController)
 
 
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 
 exports.initializeDatabase = () => {
-   return mongoose.connect('mongodb://localhost:27017/cubicle');}
+   return mongoose.connect('mongodb://localhost:27017/artGallery');}
